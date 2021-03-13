0e250746ca47cfc0117d8119b8c737dfe92bd730
Caleb Mackey <caleb.mackey@gmail.com>
2021-03-12 23:18:55 -0600

    initial list of modules that should be renamed

---
+++ src/matching.ts
@@ -0,0 +1,5 @@
+export const suggestedRenames: { [k: string]: string } = {
+  "esri/config": "esriConfig",
+  "esri/request": "esriRequest",
+  "esri/widgets/smartMapping/support/utils": "smartMappingUtils",
+}
