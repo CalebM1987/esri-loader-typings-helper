2323310257141b1f14f0a0a47575c0c3eb199d9c
Caleb Mackey <caleb.mackey@gmail.com>
2021-05-05 09:54:38 -0500

    fix(matching): fix constructor typings for geometryEngine modules, fixes #2
    this fix will remove the 'Constructor' suffix when importing the geometryEngine and
    geometryEngineAsync modules in TypeScript
    
    fix #2

--- src/matching.ts
+++ src/matching.ts
@@ -1,5 +1,7 @@
 export const suggestedRenames: { [k: string]: string } = {
   "esri/config": "esriConfig",
   "esri/request": "esriRequest",
+  "esri/geometry/geometryEngine": "geometryEngine",
+  "esri/geometry/geometryEngineAsync": "geometryEngineAsync",
   "esri/widgets/smartMapping/support/utils": "smartMappingUtils",
 }
