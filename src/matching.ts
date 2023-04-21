f5231b081f80d74031565e2436edf8a636cdb0d6
Caleb Mackey <caleb.mackey@gmail.com>
2023-04-21 14:29:03 -0500

    refactor(matching): handle IdentityManager rename

--- src/matching.ts
+++ src/matching.ts
@@ -1,6 +1,7 @@
 export const suggestedRenames: { [k: string]: string } = {
   "esri/config": "esriConfig",
   "esri/request": "esriRequest",
+  "esri/identity/IdentityManager": "identityManager",
   "esri/geometry/geometryEngine": "geometryEngine",
   "esri/geometry/geometryEngineAsync": "geometryEngineAsync",
   "esri/widgets/smartMapping/support/utils": "smartMappingUtils",
